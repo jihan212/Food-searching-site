08128da5243d574cd81e4d4f3f5732d0b8abdc7e
snapper <zihanjashim2121@gmail.comm>
2021-02-07 01:37:54 +0600

    food searching site

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-fetch ('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then (response => response.json())
-.then (data => {
-    console.log(data);
-})
+
+const foodList = food =>{
+    fetch ('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then (response => response.json())
+    .then (data =>{
+        const food = data.categories;
+        for (let i = 0; i < food.length; i++) {
+            const foodName = food[i].strCategory;
+            console.log(foodName);
+        }
+    })
+}
+foodList();
