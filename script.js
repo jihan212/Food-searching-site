393b937b01777f3f8a5ef8c767051287ccf2ef85
snapper <zihanjashim2121@gmail.comm>
2021-02-07 18:12:15 +0600

    food searching site

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ const foodMenus = foods =>{
     });
 }
 const displayDetail = name => {
-    //https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
     const url = `https://www.themealdb.com/api/json/v1/1/categories.php`
     fetch(url)
     .then(response => response.json())
