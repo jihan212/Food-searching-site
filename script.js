847f0ab740f5b58a1fe533bd2093d41ce6ffb136
snapper <zihanjashim2121@gmail.comm>
2021-02-07 10:47:25 +0600

    food searching site

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
+fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+  .then((response) => response.json())
+  .then((data) => {
+    foodMenus(data);
+  });
 
-    fetch ('https://www.themealdb.com/api/json/v1/1/categories.php')
-    .then (response => response.json())
-    .then (data =>{
-        foodMenus(data);
-        //console.log(data.categories.strCategory);
-    })
-    function foodMenus(foods) {
-        const foodItemsDiv = document.getElementById("food-items");
-        const foodItem = foods.categories;
-        for (let i = 0; i < foodItem.length; i++) {
-            const foodItems = foodItem[i];
-            const foodItemDiv = document.createElement('div');
+function foodMenus(foods) {
+  const foodItemsDiv = document.getElementById("food-items");
+  const foodItem = foods.categories;
+  for (let i = 0; i < foodItem.length; i++) {
+    const foodItems = foodItem[i];
+    const foodItemDiv = document.createElement("div");
 
-            const foodNames = document.createElement('h1');
-            foodNames.innerText = foodItems.strCategory;
-            foodItemDiv.appendChild(foodNames);
+    const foodInfo = `
+                <h1>${foodItems.strCategory}</h1>
+                <p>${foodItems.strCategoryDescription}</p>
+            `
+    foodItemDiv.innerHTML = foodInfo;
 
-            const foodDescription = document.createElement('p');
-            foodDescription.innerText = foodItems.strCategoryDescription;
-            foodItemDiv.appendChild(foodDescription);
-
-            foodItemsDiv.appendChild(foodItemDiv);
-        }
-    }
+    foodItemsDiv.appendChild(foodItemDiv);
+  }
+}
