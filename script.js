cc1261e185e153b86d361dee0f33f71fb53e8cad
snapper <zihanjashim2121@gmail.comm>
2021-02-07 10:39:22 +0600

    food searching site

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
-const foodList = food =>{
     fetch ('https://www.themealdb.com/api/json/v1/1/categories.php')
     .then (response => response.json())
     .then (data =>{
-        const food = data.categories;
-        for (let i = 0; i < food.length; i++) {
-            const foodName = food[i].strCategory;
-            console.log(foodName);
-        }
+        foodMenus(data);
+        //console.log(data.categories.strCategory);
     })
-}
-foodList();
+    function foodMenus(foods) {
+        const foodItemsDiv = document.getElementById("food-items");
+        const foodItem = foods.categories;
+        for (let i = 0; i < foodItem.length; i++) {
+            const foodItems = foodItem[i];
+            const foodItemDiv = document.createElement('div');
+
+            const foodNames = document.createElement('h1');
+            foodNames.innerText = foodItems.strCategory;
+            foodItemDiv.appendChild(foodNames);
+
+            const foodDescription = document.createElement('p');
+            foodDescription.innerText = foodItems.strCategoryDescription;
+            foodItemDiv.appendChild(foodDescription);
+
+            foodItemsDiv.appendChild(foodItemDiv);
+        }
+    }
