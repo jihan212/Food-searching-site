d8e972282c885166d9b804d732130ab16c2bb734
snapper <zihanjashim2121@gmail.comm>
2021-02-08 12:06:06 +0600

    food search site

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("search-btn").addEventListener("click", function(){
-        foodMenus(data);
-        const fooditem = document.getElementById("food-items");
-        fooditem.style.display = "grid";
-    })
-  });
-
-const foodMenus = foods =>{
-  const foodItemsDiv = document.getElementById("food-items");
-  const foodItem = foods.categories;
-    foodItem.forEach(foodItems => {
-    const foodItemDiv = document.createElement("div");
-    foodItemDiv.className="food-item";
-
-    const foodInfo = `
-                <img class="food-img" src="${foodItems.strCategoryThumb}"></img>
-                <h1 class="food-name">${foodItems.strCategory}</h1>
-                <button onClick="displayDetail('${foodItems.strCategory}')" id="recipeBTN" class="recipe-btn">Recipe</button>
-            `;
-    foodItemDiv.innerHTML = foodInfo;
-
-    foodItemsDiv.appendChild(foodItemDiv);
-    });
+document.getElementById("search-btn").addEventListener("click",function(){
+  searchFood();
+})
+const searchFood = () => {
+  const searchLetter = document.getElementById("search-input").value;
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchLetter}`
+  fetch (url)
+  .then (res => res.json())
+  .then (data => displayFoods(data.meals))
 }
-const displayDetail = name => {
-    const url = `https://www.themealdb.com/api/json/v1/1/categories.php`
-    fetch(url)
-    .then(response => response.json())
-    .then(data => recipeDetails(data.categories[0]));
-}
-const recipeDetails = recipe =>{
-    const detailDiv = document.getElementById("food-details");
-    detailDiv.innerHTML=`
-    <img class="recipe-img" src="${recipe.strCategoryThumb}"></img>
-    <h1 class="food-name">${recipe.strCategory}</h1> 
-    <p>${recipe.strCategoryDescription}</p>   
-    `
+const displayFoods = foods => {
+  const foodItems = document.getElementById("food-items");
+
+  foods.forEach(food => {
+    const foodItem = document.createElement('div');
+    foodItem.className = "food-item";
+    foodItem.innerHTML = `
+    <img class="food-img" src="${food.strMealThumb}"></img>
+    <h1 class="food-name">${food.strMeal}</h1>
+    `;
+    foodItems.appendChild(foodItem);
+  })
 }
